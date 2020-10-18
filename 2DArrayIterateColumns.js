eb410152c5fb4d83dc50d52258ec5abcef7429d2
David Agustin <dagustin@myvest.com>
2020-10-18 07:56:48 -0700

    Finish 2D Array Iterate Columns

---
+++ 2DArrayIterateColumns.js
@@ -0,0 +1,5 @@
+let twoDimensionalArray = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]];
+
+for (let i = 0; i < twoDimensionalArray[0].length; i++) {
+    twoDimensionalArray.map(singleArray => console.log(singleArray[i])); // 1, 5, 9, 2, 6, 10, 3, 7, 11, 4, 8, 12
+}
