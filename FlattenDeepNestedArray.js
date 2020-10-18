ba77caad816590b95b4b259b195a075e4791dc5d
David Agustin <dagustin@myvest.com>
2020-10-18 11:01:13 -0700

    Fix Spacing

--- FlattenDeepNestedArray.js
+++ FlattenDeepNestedArray.js
@@ -3,8 +3,6 @@ let flatArray = nestedArray.flat(Infinity);
 
 console.log(flatArray); // [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ]
 
-
-
 nestedArray = [[1, 2],[3, [4], 5], [[6, 7], [8, [9]]]]; // deeply nested example
 flatArray = nestedArray.flat(Infinity);
 
