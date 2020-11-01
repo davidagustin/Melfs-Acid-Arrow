152e3f474ea4493a1bbfdf75624c7e76523369e9
David Agustin <dagustin@myvest.com>
2020-11-01 12:19:47 -0800

    Fix Grammar Errors and Format in StringRawZip

--- StringRawZip.js
+++ StringRawZip.js
@@ -15,20 +15,22 @@ console.log(`zipped: ${zipped}`); // 't0e1s2t'
     returned from String.raw is of course a string.
  */
 
-// value of raw array must more elements than its substitution
+// value of raw array must have more elements than its substitution
 zipped = String.raw({raw: [1, 2, 3]}, 4, 5) // notice raw has more elements than its substitution
 console.log(`zipped: ${zipped}`); // 14253 ;
 
 /*
     zipMalfunction will be an unwanted result because the
-    array length in raw is equal to the amount of arguments passed
+    array length in raw is equal to the amount of arguments passed from the second argument to the end
 */
 const zipMalfunction = String.raw({raw: [1, 2, 3]}, 4, 5, 6)
 console.log(`zipMalfunction: ${zipMalfunction}`); // 1, 4, 2, 5, 3 ; 6 is ignored
 
 /*
+	We want to zip two arrays with example:
     [1, 2, 3] and [4, 5, 6, 7, 8] => '14253678'
-    to work around this problem String.padEnd() will be used
+
+    To work around the previous problem, String.padEnd() will be used
 */
 
 const substitutionArguments = [4, 5, 6, 7, 8]; // raw <= substitutions
@@ -37,6 +39,7 @@ let rawArray = [1, 2, 3];
 /*
     The comma in padEnd will be used to split and substitutionArguments.length is added by 1 because raw must have more elements than its substitution
  */
+ 
 if (rawArray <= substitutionArguments) {
     rawArray = rawArray.join('').padEnd(substitutionArguments.length + 1, ',').split('');
 }
