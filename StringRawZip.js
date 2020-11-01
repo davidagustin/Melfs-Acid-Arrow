59c81496ecea927698dd1faeadb4ba9d25774f6b
David Agustin <dagustin@myvest.com>
2020-11-01 12:13:42 -0800

    Remove Array in Spread Operator in StringRawZip

--- StringRawZip.js
+++ StringRawZip.js
@@ -16,7 +16,7 @@ console.log(`zipped: ${zipped}`); // 't0e1s2t'
  */
 
 // value of raw array must more elements than its substitution
-zipped = String.raw({raw: [1, 2, 3]}, 4, 5) //notice raw has more elements than its substitution
+zipped = String.raw({raw: [1, 2, 3]}, 4, 5) // notice raw has more elements than its substitution
 console.log(`zipped: ${zipped}`); // 14253 ;
 
 /*
@@ -27,23 +27,25 @@ const zipMalfunction = String.raw({raw: [1, 2, 3]}, 4, 5, 6)
 console.log(`zipMalfunction: ${zipMalfunction}`); // 1, 4, 2, 5, 3 ; 6 is ignored
 
 /*
-    [1, 2, 3] and [4, 5, 6, 7, 8] => '14567823'
+    [1, 2, 3] and [4, 5, 6, 7, 8] => '14253678'
     to work around this problem String.padEnd() will be used
 */
 
 const substitutionArguments = [4, 5, 6, 7, 8]; // raw <= substitutions
 let rawArray = [1, 2, 3];
 
-
+/*
+    The comma in padEnd will be used to split and substitutionArguments.length is added by 1 because raw must have more elements than its substitution
+ */
 if (rawArray <= substitutionArguments) {
-    rawArray = rawArray.join('').padEnd(substitutionArguments.length + 1, ',').split(''); // the comma in padEnd will be used to split
+    rawArray = rawArray.join('').padEnd(substitutionArguments.length + 1, ',').split('');
 }
 
-zipped = String.raw({raw: [...rawArray]}, ...[substitutionArguments]).split(',').join(''); // remove comma from raw String by split and rejoin
-console.log(`zipped: ${zipped}`); // 14567823
+zipped = String.raw({raw: [...rawArray]}, ...substitutionArguments).split(',').join(''); // remove comma from raw String by split and rejoin
+console.log(`zipped: ${zipped}`); // 14253678
 
 zipped = zipped.split(''); // turn string into an array
-console.log(`zipped: ${zipped}`); // ['1', '4', '5', '6', '7', '8', '2', '3']
+console.log(`zipped: ${zipped}`); // ['1', '4', '2', '5', '3', '6', '7', '8']
 
 zipped = zipped.map((stringElement) => Number(stringElement)); // change string number to datatype number
-console.log(`zipped: ${zipped}`); // [1, 4, 5, 6, 7, 8, 2, 3]
+console.log(`zipped: ${zipped}`); // [1, 4, 2, 5, 3, 6, 7, 8]
